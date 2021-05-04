1dc0151ce6002f9a16fd5081d6377339c05cc350
AmrAhmedAli <amrr.ali123@gmail.com>
2021-05-04 05:51:41 +0200

    Left col banks list implemented
    Change-Id: I2283365a4fc82f299a6570f4f6d3c2e3447d672c

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  filter: string;
+  selectedBanks: Array<string> = [null, null, null];
+  len: number = 0;
+  constructor() {}
 
   ngOnInit(): void {
+    this.filter = '1';
   }
+  submit(value: string): void {
+    if (this.selectedBanks.includes(value)) {
+      this.len -= 1;
+      let idx = this.selectedBanks.indexOf(value);
+      this.selectedBanks[idx] = null;
+      document.getElementById(value + '-bank').style.removeProperty('border');
+      document.getElementById(value + '-polygon').hidden = true;
+      for (let i = idx + 1; i < 3; i++) {
+        if (this.selectedBanks[i] != null) {
+          this.selectedBanks[i - 1] = this.selectedBanks[i];
 
+          document.getElementById(this.selectedBanks[i] + '-text').innerHTML =
+            ' ' + i;
+          this.selectedBanks[i] = null;
+        } else {
+          break;
+        }
+      }
+    } else {
+      if (this.selectedBanks.includes(null)) {
+        this.len += 1;
+        this.selectedBanks[this.selectedBanks.indexOf(null)] = value;
+        document.getElementById(value + '-bank').style.border = '1px solid';
+        document.getElementById(value + '-polygon').hidden = false;
+        document.getElementById(value + '-text').innerHTML = ' ' + this.len;
+      }
+    }
+  }
 }
